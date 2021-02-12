1f6b728e71cd24daa3d94fa2bb43b1834620419b
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2021-02-12 18:48:29 +0530

    Timeline popper trigger and placement functional.

--- components/mindmap/Timeline.js
+++ components/mindmap/Timeline.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef } from 'react'
-import { Spinner } from 'reactstrap'
+import React, { useEffect, useRef, useState } from 'react'
+import {
+  Popover, PopoverHeader, Spinner, Card, CardBody, CardText, PopoverBody
+} from 'reactstrap'
 import { Timeline as VisTimeline } from 'vis-timeline'
 import { useUser } from '../../utils/auth/useUser'
-import fetchWrapper from '../../utils/fetchWrapper'
+import fetchWrapper, { fetcher } from '../../utils/fetchWrapper'
+import { get } from 'lodash'
 
 export default function timeline ({ mkey }) {
   const { user } = useUser()
@@ -29,6 +32,31 @@ export default function timeline ({ mkey }) {
 
 const Timeline = ({ data }) => {
   const timelineRef = useRef(null)
+  const [popoverOpen, setPopoverOpen] = useState(false)
+  const [target, setTarget] = useState('timeline')
+  const [offset, setoffset] = useState(0)
+
+  const toggle = () => {
+    if (target === 'timeline') {
+      setPopoverOpen(false)
+    }
+    else {
+      setPopoverOpen(!popoverOpen)
+
+      // Popover state will toggle on next tick. So if we want a code block to run for the to-be
+      // open state, we must check for popoverOpen === false!
+      if (!popoverOpen) {
+        const timeline = timelineRef.current
+        const targetRef = document.querySelector(`[data-id="${target}"]`)
+        const boundingRect = targetRef.getBoundingClientRect()
+        const mid = boundingRect.x + boundingRect.width / 2
+        const offset = timeline.getBoundingClientRect().x - mid
+
+        setoffset(offset)
+      }
+    }
+  }
+
   const items = data.map((event, idx) => ({
     id: idx,
     className: event.event,
@@ -47,12 +75,13 @@ const Timeline = ({ data }) => {
     cluster: {
       titleTemplate: '{count}',
       maxItems: 1,
-      clusterCriteria: () => true,
       showStipes: true,
       fitOnDoubleClick: true
     },
     max: items[items.length - 1].start + margin,
-    min: items[0].start - margin
+    min: items[0].start - margin,
+    selectable: false,
+    dataAttributes: ['id']
   }
 
   useEffect(() => {
@@ -64,20 +93,65 @@ const Timeline = ({ data }) => {
     )
 
     timeline.on('doubleClick', (properties) => {
-      const { item, isCluster } = properties
+      const { what, item, isCluster } = properties
 
-      if (item === null) {
-        timeline.fit()
-      }
-      else if (!isCluster) {
-        timeline.focus(item)
+      switch (what) {
+        case 'background':
+          timeline.fit()
+
+          break
+        case 'item':
+          if (!isCluster) {
+            timeline.focus(item)
+          }
+
+          break
       }
     })
+
+    timeline.on('click', (properties => {
+      const { what, isCluster, item } = properties
+
+      if (what === 'item' && !isCluster) {
+        setTarget(item)
+        console.log(data[item])
+      }
+      else {
+        setTarget('timeline')
+      }
+    }))
 
     return () => timeline.destroy()
   }, [data.length])
 
   return <div className={'border border-secondary rounded'}>
     <div id={'timeline'} ref={timelineRef} className={'m-1'}/>
+    <Popover target="timeline" isOpen={popoverOpen} toggle={toggle} offset={offset}
+             boundariesElement={'timeline'} placement={'top-start'}>
+      <PopoverHeader>Event: {get(data, [target, 'event'], 'NA')}</PopoverHeader>
+      <PopoverBody>
+        <Card
+          className="border-dark"
+          style={{ minWidth: '50vw', maxWidth: '90vw' }}
+        >
+          <CardBody>
+            <CardText tag="div" className="mw-100">
+              <EventDetail event={data[target]}/>
+            </CardText>
+          </CardBody>
+        </Card>
+      </PopoverBody>
+    </Popover>
   </div>
+}
+
+const EventDetail = ({ event }) => {
+  const { user } = useUser()
+
+  if (!event) {
+    return null
+  }
+  else {
+    return JSON.stringify(event, null, 2)
+  }
 }
