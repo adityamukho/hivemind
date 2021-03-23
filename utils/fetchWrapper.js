c5ad238a7ed6b1f1f1a5ca3d351ab4f846545cd7
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2021-03-23 08:28:22 +0530

    Reverting Pace.track() as it has no impact on Chrome family browsers.

--- utils/fetchWrapper.js
+++ utils/fetchWrapper.js
@@ -1,16 +1,15 @@
 import useSWR from 'swr'
 
-export const fetcher = async (url, token, method = 'GET', body) =>
-  await Pace.track(async () => {
-    const res = await fetch(url, {
-      method,
-      headers: new Headers({ 'Content-Type': 'application/json', token }),
-      credentials: 'same-origin',
-      body
-    })
-
-    return { data: await res.json(), status: res.status, ok: res.ok }
+export const fetcher = async (url, token, method = 'GET', body) => {
+  const res = await fetch(url, {
+    method,
+    headers: new Headers({ 'Content-Type': 'application/json', token }),
+    credentials: 'same-origin',
+    body
   })
+
+  return { data: await res.json(), status: res.status, ok: res.ok }
+}
 
 const fetchWrapper = (user, url) => useSWR(
   user ? [url, user.token] : null,
