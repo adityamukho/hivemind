e3ab44ab58efbe9d20c7ce72e67a86bdb523cf81
ksj18 <55830874+ksj18@users.noreply.github.com>
2021-03-27 20:02:34 +0000

    Update help.js

--- pages/help.js
+++ pages/help.js
@@ -3,10 +3,9 @@ import Link from 'next/link'
 const Page = () => {
   return (
     <div>
-      <p>
-        This page is static because it does not fetch any data or include the
-        authed user info.
-      </p>
+    
+    <h2>Main Menu</h2>
+   
       <Link href={'/'}>
         <a>Home</a>
       </Link>
