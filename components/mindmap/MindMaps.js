e81b2290f2bc401f80d6408f6c5a2cb3ab0cb7dc
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2021-04-01 19:46:43 +0530

    Replace mindmaps list by table.

--- components/mindmap/MindMaps.js
+++ components/mindmap/MindMaps.js
@@ -1,23 +1,71 @@
 import Link from 'next/link'
 import React from 'react'
-import { ListGroup, ListGroupItem } from 'reactstrap'
+import BootstrapTable from 'react-bootstrap-table-next'
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter'
+// import { Trash2 } from 'react-feather'
+
+// function getOps(entry) {
+//   const output = []
+//
+//   if (['admin', 'write'].includes(entry.access.access)) {
+//     output.push(
+//       <span className={'mx-1 text-danger'} key={entry.mindmap._key}>
+//         <Trash2 />
+//       </span>
+//     )
+//   }
+//
+//   return output
+// }
 
 const MindMaps = ({ data }) => {
   if (data.length) {
+    const columns = [
+      {
+        dataField: 'title',
+        text: 'Title',
+        sort: true,
+        filter: textFilter(),
+        formatter: (cell, row) => (
+          <Link href="/mmaps/[id]" as={`/mmaps/${row.id}`}>
+            <a>{cell}</a>
+          </Link>
+        ),
+      },
+      {
+        dataField: 'access',
+        text: 'Access',
+        sort: true,
+        filter: textFilter(),
+      },
+      // {
+      //   dataField: 'ops',
+      //   sort: false,
+      //   text: ''
+      // },
+    ]
+
+    const rows = data.map((entry) => {
+      return {
+        id: entry.mindmap._key,
+        access: entry.access.access,
+        title: entry.mindmap.name,
+        // ops: getOps(entry),
+      }
+    })
+
     return (
-      <ListGroup>
-        {data.map((entry) => (
-          <ListGroupItem key={entry.mindmap._key}>
-            <Link href="/mmaps/[id]" as={`/mmaps/${entry.mindmap._key}`}>
-              <a>{entry.mindmap.name}</a>
-            </Link>
-            &nbsp;
-            <span className="text-muted float-right">
-              <small>Access: {entry.access.access}</small>
-            </span>
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      <BootstrapTable
+        bootstrap4
+        keyField="id"
+        data={rows}
+        columns={columns}
+        hover
+        condensed
+        filter={filterFactory()}
+        defaultSorted={[{ dataField: 'title', order: 'asc' }]}
+        defaultSortDirection={'asc'}
+      />
     )
   }
 
